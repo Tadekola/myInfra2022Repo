dd8b09d5bafd5e097085a28e093903111ec2073b
Tadekola <86187549+Tadekola@users.noreply.github.com>
2022-05-12 19:47:04 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,12 @@
 terraform {
+  required_providers {
+
+    aws = "~> 3.74"
+  }
   backend "s3" {
     bucket = "my-s3bucket-20220513002320204000000001"
-    key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    key 
+    dynamodb_ta= "main"
+    region = "us-east-1"ble = "my-dynamodb-table"
   }
 }
